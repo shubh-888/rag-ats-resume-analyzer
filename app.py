192f672b6c4f5ee52fee39362385701163477231
Shubham Maddhesiya <shubhammaddhesiya45@gmail.com>
2026-06-01 02:02:59 +0530

    Fix Render port binding

--- app.py
+++ app.py
@@ -517,7 +517,12 @@ def download():
 
 # ---------------- RUN ---------------- #
 
+# if __name__ == "__main__":
+#     app.run(
+#         debug=True
+#     )
+import os
+
 if __name__ == "__main__":
-    app.run(
-        debug=True
-    )
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
